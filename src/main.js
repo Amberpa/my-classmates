ffac5e4705f1819d795a31727e8cd87096f0532a
Abby Cloete <932400748@acgedu.com>
2020-02-25 17:39:45 +1300

    abby screen

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import Abby from './components/abby/Abby.vue';
+
 
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/abby',
+    component: Abby
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
